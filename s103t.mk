34bf49517628b5334424e4c1a2a9224d847cb91e
Robill Tian Supatrio <rsupatrio@android-x86.org>
2012-01-02 15:56:14 +0800

    update for honeycomb-x86

--- s103t.mk
+++ s103t.mk
@@ -1,6 +1,9 @@
 PRODUCT_PACKAGES := $(THIRD_PARTY_APPS)
 PRODUCT_PACKAGES += sensors.$(TARGET_PRODUCT) lights.$(TARGET_PRODUCT)
 
+PRODUCT_COPY_FILES := \
+	$(LOCAL_PATH)/modprobe.conf:system/etc/modprobe.conf
+
 $(call inherit-product,$(SRC_TARGET_DIR)/product/generic_x86.mk)
 
 PRODUCT_NAME := s103t
@@ -8,7 +11,3 @@ PRODUCT_DEVICE := s103t
 PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_MODEL := Ideapad S10-3T
 PRODUCT_BRAND := Lenovo
-#PRODUCT_LOCALES := de_DE en_GB
-
-PRODUCT_COPY_FILES := \
-	$(LOCAL_PATH)/modprobe.conf:system/etc/modprobe.conf
