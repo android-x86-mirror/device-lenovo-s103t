3ba46602c21a48acc9e3a0df342902f8379de6e3
Robill Tian Supatrio <rsupatrio@android-x86.org>
2012-01-08 12:59:50 +0800

    Update for ics-x86.

--- s103t.mk
+++ s103t.mk
@@ -1,7 +1,8 @@
-PRODUCT_PACKAGES := $(THIRD_PARTY_APPS)
-PRODUCT_PACKAGES += sensors.$(TARGET_PRODUCT) lights.$(TARGET_PRODUCT)
+PRODUCT_PACKAGES += sensors.$(TARGET_PRODUCT) lights.$(TARGET_PRODUCT) 
+PRODUCT_PACKAGES += audio.primary.$(TARGET_PRODUCT) audio_policy.$(TARGET_PRODUCT) audio.a2dp.default alsa.$(TARGET_PRODUCT)
+PRODUCT_PACKAGES += camera.$(TARGET_PRODUCT) Camera
 
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/modprobe.conf:system/etc/modprobe.conf
 
 $(call inherit-product,$(SRC_TARGET_DIR)/product/generic_x86.mk)
