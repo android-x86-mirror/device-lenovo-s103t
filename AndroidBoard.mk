b190af8f9988112110282e04591561b5c0ee5055
Robill Tian Supatrio <rsupatrio@android-x86.org>
2012-01-14 22:53:48 +0800

    Replace wl with brcmsmac driver.

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-
+LOCAL_FIRMWARES := $(subst device/common/firmware/,,$(wildcard device/common/firmware/brcm/*)) $(notdir $(wildcard device/common/firmware/iwlwifi-*))
 TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/s103t_defconfig
 TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/s103t_info
 TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
