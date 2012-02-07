2452b28b3135ed9d25d32e733fbae11f0448cabd
Robill Tian Supatrio <rsupatrio@android-x86.org>
2012-02-08 00:41:57 +0800

    Use camera.default and liblights.default.

--- s103t.mk
+++ s103t.mk
@@ -1,6 +1,6 @@
-PRODUCT_PACKAGES += sensors.$(TARGET_PRODUCT) lights.$(TARGET_PRODUCT) 
+PRODUCT_PACKAGES += sensors.$(TARGET_PRODUCT)
 PRODUCT_PACKAGES += audio.primary.$(TARGET_PRODUCT) audio_policy.$(TARGET_PRODUCT) audio.a2dp.default alsa.$(TARGET_PRODUCT)
-PRODUCT_PACKAGES += camera.$(TARGET_PRODUCT) Camera AndroidTerm FileManager
+PRODUCT_PACKAGES += AndroidTerm FileManager
 
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/modprobe.conf:system/etc/modprobe.conf
